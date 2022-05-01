87f1ed73d9df9c213fee5dd9a292016f4688e55f
lllllll20 <lllllll@mailbox.org>
2022-05-01 12:11:26 +0100

    resolve symlinks backuptohd

--- system/backuptohd.sh
+++ system/backuptohd.sh
@@ -6,7 +6,7 @@ rsync -av --delete "${HOME}"/work_docs/  /run/media/iain/LaCie/work_docs/
 rsync -av --exclude 'Films' --exclude 'virtual machines' --exclude 'Games' --delete /run/media/iain/samsung/media/*  /run/media/iain/LaCie/media/
 rsync -av /run/media/iain/samsung/media/Films/  /run/media/iain/LaCie/media/Films/
 rsync -av --delete "${HOME}"/notes/  /run/media/iain/LaCie/notes/
-rsync -av --delete "${HOME}"/scripts/  /run/media/iain/LaCie/scripts/
+rsync -avL --delete "${HOME}"/scripts/  /run/media/iain/LaCie/scripts/
 
 
 
