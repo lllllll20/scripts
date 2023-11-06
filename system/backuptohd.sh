b73fe6a0cf19fbf706366e0873e6a13ae4cbfe9c
lllllll20 <lllllll@mailbox.org>
2023-11-06 07:42:25 +0000

    changes

--- system/backuptohd.sh
+++ system/backuptohd.sh
@@ -2,16 +2,16 @@
 
 # Delete files from hard drive if they are deleted from desktop
 
-rsync -av --delete --exclude 'git' "${HOME}"/computing/  /run/media/iain/LaCie/computing/
-rsync -av --delete "${HOME}"/my_docs/  /run/media/iain/LaCie/my_docs/
-rsync -av --delete "${HOME}"/work_docs/  /run/media/iain/LaCie/work_docs/
-rsync -av --exclude 'Films' --exclude 'virtual machines' --exclude 'Games' --delete /run/media/iain/samsung/media/  /run/media/iain/LaCie/media/
-rsync -av --delete "${HOME}"/notes/  /run/media/iain/LaCie/notes/
-rsync -avL --delete "${HOME}"/scripts/  /run/media/iain/LaCie/scripts/
+rsync -av --delete --exclude 'git' "${HOME}"/computing/  /run/media/"${USER}"/LaCie/computing/
+rsync -av --delete "${HOME}"/my_docs/  /run/media/"${USER}"/LaCie/my_docs/
+rsync -av --delete "${HOME}"/work_docs/  /run/media/"${USER}"/LaCie/work_docs/
+rsync -av --exclude 'Films' --exclude 'Games' --delete "${HOME}"/media/  /run/media/"${USER}"/LaCie/media/
+rsync -av --delete "${HOME}"/notes/  /run/media/"${USER}"/LaCie/notes/
+rsync -avL --delete "${HOME}"/scripts/  /run/media/"${USER}"/LaCie/scripts/
 
 
 # Film and email directory is handled separately so that films are never lost, but can be deleted from desktop
 
-rsync -av /run/media/iain/samsung/media/Films/  /run/media/iain/LaCie/media/Films/ 
-rsync -av "${HOME}"/.archivedmail/ /run/media/iain/LaCie/.archivedmail/
+rsync -av "${HOME}"/media/Films/  /run/media/"${USER}"/LaCie/media/Films/ 
+rsync -av "${HOME}"/.archivedmail/ /run/media/"${USER}"/LaCie/.archivedmail/
 
